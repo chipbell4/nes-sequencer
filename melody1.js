ad90854cd09079c2a9ace6f5f50053b8fc2b5e6b
Chip Bell <cbell@pbs.org>
2016-04-03 18:02:11 -0400

    Fleshing out the melody

--- melody1.js
+++ melody1.js
@@ -1,11 +1,35 @@
 var MELODY1 = (function() {
+  var scaleTones = ['C#5', 'D#5', 'E5', 'F#5', 'G#5', 'A#6', 'B6', 'C#6', 'D#6', 'E6'].map(MusicTools.frequency);
+
+  var phrase1 = [
+    2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 0, 2, 4, 3, 2
+  ];
+  var phrase2 = [
+    2, 2, 2, 2, 2, 2, 0, 4, 3, 2, 2, 2,
+  ];
+  var phrase3 = [
+    2, 2, 2, 2, 2, 2, 0, 4, 3, 2, 1, 0,
+  ];
+  var phrase4 = [
+    0, 4, 6, 5, 0, 4, 6, 5, 8, 9
+  ];
+
+  var fullMelody = phrase1.concat(phrase2).concat(phrase3).concat(phrase4);
+  var melodyIndex = 0;
+
   var raw = RHYTHM.map(function(note) {
+    var frequency = 0;
+    if(note.volume > 0) {
+      frequency = scaleTones[fullMelody[melodyIndex]];
+      melodyIndex++;
+    }
+
     return {
       duration: note.duration,
       volume: note.volume,
-      frequency: 440
+      frequency: frequency
     };
   });
 
-  return Staccato(raw);
+  return Staccato(raw.concat(raw));
 })();
