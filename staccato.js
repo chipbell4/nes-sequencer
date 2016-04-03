2604cf79f5b21012a0ca09fcb683825b2520a6fd
Chip Bell <cbell@pbs.org>
2016-04-03 18:29:35 -0400

    Making staccato slightly more parameterizable

--- staccato.js
+++ staccato.js
@@ -1,15 +1,16 @@
 var Staccato = function(melody) {
+  var noteReduction = 0.3;
   var newMelody = [];
   for(var i = 0; i < melody.length; i++) {
     newMelody.push({
       frequency: melody[i].frequency,
-      duration: melody[i].duration / 2,
+      duration: melody[i].duration * noteReduction,
       volume: melody[i].volume
     });
     
     newMelody.push({
       frequency: melody[i + 1] ? melody[i+1].frequency : melody[i].frequency,
-      duration: melody[i].duration / 2,
+      duration: melody[i].duration * (1 - noteReduction),
       volume: 0
     });
   }
