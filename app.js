d8de575618814a60a184f3182eb3192ce1420809
Chip Bell <cbell@pbs.org>
2016-04-03 17:17:16 -0400

    Moving bass line to separate file

--- app.js
+++ app.js
@@ -7,28 +7,6 @@ var aBass = MusicTools.frequency('A3');
 var eighth = MusicTools.duration(tempo, 0.5);
 var sixteenth = MusicTools.duration(tempo, 0.25);
 
-var riffOne = [
-  { frequency: cSharpBass, volume: 0.2, duration: eighth },
-  { frequency: cSharpBass, volume: 0.2, duration: sixteenth },
-  { frequency: cSharpBass, volume: 0.2, duration: sixteenth },
-];
-// make staccato, then into an eight beat lick
-riffOne = Staccato(riffOne);
-riffOne = riffOne.concat(riffOne);
-riffOne = riffOne.concat(riffOne);
-riffOne = riffOne.concat(riffOne); // eight beats of the riff
-
-var riffTwo = riffOne.map(function(note) {
-  return { frequency: aBass, duration: note.duration, volume: note.volume };
-});
-
-var riffThree = riffOne.map(function(note) {
-  return { frequency: bBass, duration: note.duration, volume: note.volume };
-});
-
-var fullBassRiff = riffOne.concat(riffTwo).concat(riffThree).concat(riffOne);
-fullBassRiff = fullBassRiff.concat(fullBassRiff);
-
 var drumRiff = [
   { frequency: 8000, volume: 0.2, duration: eighth },
   { frequency: 8000, volume: 0.2, duration: sixteenth },
@@ -44,5 +22,5 @@ fullDrumRiff = fullDrumRiff.concat(fullDrumRiff);
 fullDrumRiff = fullDrumRiff.concat(fullDrumRiff);
 fullDrumRiff = fullDrumRiff.concat(fullDrumRiff);
 
-NesSequencer.scheduleMelody(NesSequencer.OSCILLATOR_TYPES.TRI, fullBassRiff)
+NesSequencer.scheduleMelody(NesSequencer.OSCILLATOR_TYPES.TRI, BASS)
 NesSequencer.scheduleMelody(NesSequencer.OSCILLATOR_TYPES.NOISE, fullDrumRiff)
---
+++ bass.js
@@ -0,0 +1,32 @@
+var BASS = (function() {
+  var tempo = 160;
+  var cSharpBass = MusicTools.frequency('C#3');
+  var bBass = MusicTools.frequency('B3');
+  var aBass = MusicTools.frequency('A3');
+  var eighth = MusicTools.duration(tempo, 0.5);
+  var sixteenth = MusicTools.duration(tempo, 0.25);
+
+  var riffOne = [
+    { frequency: cSharpBass, volume: 0.2, duration: eighth },
+    { frequency: cSharpBass, volume: 0.2, duration: sixteenth },
+    { frequency: cSharpBass, volume: 0.2, duration: sixteenth },
+  ];
+  // make staccato, then into an eight beat lick
+  riffOne = Staccato(riffOne);
+  riffOne = riffOne.concat(riffOne);
+  riffOne = riffOne.concat(riffOne);
+  riffOne = riffOne.concat(riffOne); // eight beats of the riff
+
+  var riffTwo = riffOne.map(function(note) {
+    return { frequency: aBass, duration: note.duration, volume: note.volume };
+  });
+
+  var riffThree = riffOne.map(function(note) {
+    return { frequency: bBass, duration: note.duration, volume: note.volume };
+  });
+
+  var fullBassRiff = riffOne.concat(riffTwo).concat(riffThree).concat(riffOne);
+  fullBassRiff = fullBassRiff.concat(fullBassRiff);
+
+  return fullBassRiff;
+})();
