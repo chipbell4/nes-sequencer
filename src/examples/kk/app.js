50558d1063155c5e9904815dcb4c548d430bbf8a
Chip Bell <cbell@pbs.org>
2016-04-14 14:09:00 -0400

    Whoops

--- src/examples/kk/app.js
+++ src/examples/kk/app.js
@@ -3,6 +3,6 @@ var KK = {
   // Some constants for common durations
   T: NES.MusicTools.duration(194, 1 / 3),
   D: NES.MusicTools.duration(194, 2 / 3),
-  Q: NES.MusicTools.durations(194, 1),
+  Q: NES.MusicTools.duration(194, 1),
   melody: { }
 };
