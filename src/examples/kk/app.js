be537527fe7ce7721a85fee42ab0367cf8ceb184
Chip Bell <cbell@pbs.org>
2016-04-13 23:19:33 -0400

    Getting tempo and constants going

--- src/examples/kk/app.js
+++ src/examples/kk/app.js
@@ -1,4 +1,7 @@
 var KartKingdom = {
-  tempo: 98,
+  tempo: 194,
+  // Some constants for common durations
+  T: NES.MusicTools.duration(194, 1 / 3),
+  D: NES.MusicTools.duration(194, 2 / 3),
   melody: { }
 };
