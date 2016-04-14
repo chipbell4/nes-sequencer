f5308891e956e7962f35dff0e8f1896176951db9
Chip Bell <cbell@pbs.org>
2016-04-13 20:05:29 -0400

    Pulling code over to sequencer module. Now to convert megaman over

--- src/sequencer.js
+++ src/sequencer.js
@@ -1,9 +1,81 @@
 NES.Sequencer = (function() {
   var sequencerInterval = null;
 
+  var calculateStartTimes = function(melodies) {
+    var startTimes = {};
+    Object.keys(melodies).forEach(function(key) {
+      startTimes[key] = [];
+      var runningSum = 0;
+      melodies[key].forEach(function(note) {
+        startTimes[key].push(runningSum);
+        runningSum += note.duration;
+      });
+    });
+
+    return startTimes;
+  };
+
+  var initializePitches = function(melodies) {
+    Object.keys(melodies).forEach(function(key) {
+      NES.Oscillators.setPitch(key, melodies[key][0].frequency, melodies[key][0].volume);
+    });
+  };
+
   return {
-    start: function(melody) {
+    /**
+     * Schedules a melody to be played. melodies, in this case is an object, where the keys are the oscillator type
+     * (NesSequencer.OSCILLATOR_TYPES.PWM1), and the associated values is an array of objects, each looking like this:
+     * { frequency: 440, volume: 0.2, duration: 1000 }, where frequency is in Hz, volume ranges between 0 and 1, and
+     * duration is in millis.
+     */
+    start: function(melodies) {
       this.stop();
+
+      var startTimes = calculateStartTimes(melodies);
+      var currentNoteIndices = {};
+      var currentNoteIndices = Object.keys(melodies).forEach(function(key) {
+        currentNoteIndices[key] = 0;
+      });
+
+      initializePitches(melodies);
+
+      var previousFrame, currentFrame, timeElapsed = 0;
+      sequencerInterval = setInterval(function() {
+         
+        // update frame time
+        if(previousFrame === undefined) {
+          previousFrame = currentFrame = Date.now();
+        }
+        timeElapsed += currentFrame - previousFrame;
+        previousFrame = currentFrame;
+        currentFrame = Date.now();
+
+        Object.keys(melodies).forEach(function(key) {
+          var k = currentNoteIndices[key];
+          var endTimeForCurrentNote = startTimes[key][k] + melodies[key][k].duration;
+          var finishedCurrentNote = endTimeForCurrentNote < timeElapsed;
+
+          // if we're at the last note, and we're past the last duration, turn off
+          if(melodies[key][k+1] === undefined && finishedCurrentNote) {
+            sequencer.setPitch(key, 440, 0);
+            return;
+          }
+          
+          // if we're at the last note, break out
+          if(melodies[key][k+1] === undefined) {
+            return;
+          }
+          
+          // if we haven't crossed into the next note, we've nothing to do
+          if(!finishedCurrentNote) {
+            return;
+          }
+
+          // otherwise, our time has just crossed over into the next note, update everything
+          currentNoteIndices[key]++;
+          NES.Oscillators.setPitch(key, melodies[key][k+1].frequency, melodies[key][k+1].volume);
+        });
+      }, 10);
     },
 
     stop: function() {
