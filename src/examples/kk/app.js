cdaea361d1e370b3ef23579ca5eb00aabdb05138
Chip Bell <cbell@pbs.org>
2016-04-13 23:24:49 -0400

    Quick rename

--- src/examples/kk/app.js
+++ src/examples/kk/app.js
@@ -1,7 +1,8 @@
-var KartKingdom = {
+var KK = {
   tempo: 194,
   // Some constants for common durations
   T: NES.MusicTools.duration(194, 1 / 3),
   D: NES.MusicTools.duration(194, 2 / 3),
+  Q: NES.MusicTools.durations(194, 1),
   melody: { }
 };
