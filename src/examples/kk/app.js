6ac6938b912ca53b16fce368a8dd1f31fe71312d
Chip Bell <cbell@pbs.org>
2016-04-14 14:18:24 -0400

    Fiddling with chord structure

--- src/examples/kk/app.js
+++ src/examples/kk/app.js
@@ -1,6 +1,7 @@
 var KK = {
   tempo: 194,
   // Some constants for common durations
+  S: NES.MusicTools.duration(194, 1 / 6),
   T: NES.MusicTools.duration(194, 1 / 3),
   D: NES.MusicTools.duration(194, 2 / 3),
   Q: NES.MusicTools.duration(194, 1),
---
+++ src/examples/kk/chords.js
@@ -0,0 +1,52 @@
+KK.melody.PWM2 = (function() {
+  var rest = function(durationName) {
+    return { frequency: 0, volume: 0, duration: KK[durationName] };
+  };
+
+  var chords = {
+    E: ['E5', 'G#5', 'B5'].map(NES.MusicTools.frequency),
+    A: ['E5', 'A5', 'C#6'].map(NES.MusicTools.frequency),
+    D: ['F#5', 'A5', 'D6'].map(NES.MusicTools.frequency),
+    B: ['F#5', 'B5', 'D#6'].map(NES.MusicTools.frequency)
+  };
+
+  var melody = [];
+
+  // Emaj lick
+  melody = melody.concat(NES.Effects.Arpeggio(chords.E, KK.T, 0.2));
+  melody.push(rest('D'));
+  melody = melody.concat(NES.Effects.Arpeggio(chords.E, KK.T, 0.2));
+  melody.push(rest('D'));
+  melody = melody.concat(NES.Effects.Arpeggio(chords.E, KK.Q, 0.2));
+  melody.push(rest('D'));
+
+  // Amaj
+  melody = melody.concat(NES.Effects.Arpeggio(chords.A, KK.T, 0.2));
+  melody.push(rest('D'));
+  melody = melody.concat(NES.Effects.Arpeggio(chords.A, KK.T, 0.2));
+  melody.push(rest('D'));
+  melody = melody.concat(NES.Effects.Arpeggio(chords.A, KK.S, 0.2));
+  melody.push(rest('S'));
+  melody = melody.concat(NES.Effects.Arpeggio(chords.A, KK.Q, 0.2));
+  melody.push(rest('Q'));
+
+  // Dmaj
+  melody = melody.concat(NES.Effects.Arpeggio(chords.D, KK.T, 0.2));
+  melody.push(rest('D'));
+  melody = melody.concat(NES.Effects.Arpeggio(chords.D, KK.T, 0.2));
+  melody.push(rest('D'));
+  melody = melody.concat(NES.Effects.Arpeggio(chords.D, KK.Q, 0.2));
+  melody.push(rest('D'));
+  
+  // Bmaj
+  melody = melody.concat(NES.Effects.Arpeggio(chords.B, KK.T, 0.2));
+  melody.push(rest('D'));
+  melody = melody.concat(NES.Effects.Arpeggio(chords.B, KK.T, 0.2));
+  melody.push(rest('D'));
+  melody = melody.concat(NES.Effects.Arpeggio(chords.B, KK.S, 0.2));
+  melody.push(rest('S'));
+  melody = melody.concat(NES.Effects.Arpeggio(chords.B, KK.Q, 0.2));
+  melody.push(rest('Q'));
+
+  return melody;
+})();
