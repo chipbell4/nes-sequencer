5b2dbbea0a908e70653565f29e56126c0a4bb281
Chip Bell <cbell@pbs.org>
2016-04-15 13:58:05 -0400

    Fleshing out the melody some more

--- src/examples/kk/melody.js
+++ src/examples/kk/melody.js
@@ -5,9 +5,13 @@ KK.melody.PWM1 = (function() {
 
   var melody = [];
 
-  var rhythm1 = ['D', 'T', 'T', 'D', 'T', 'D', 'Q', 'D', 'T', 'Q', 'Q', 'Q'];
-  var melodyLine1 = ['E5', 'D#5', 'E5', '', 'B4', '', 'G#4', 'A4', 'B4', '', '', ''];
-  var melodyLine2 = ['D5', 'C#5', 'D5', '', 'E5', '', 'C#5', 'A4', 'B4', '', '', ''];
+  var rhythm1 =     [ 'D',   'T', 'T', 'D',   'T', 'D',  'Q',  'D',  'T', 'Q', 'Q', 'Q'];
+  var melodyLine1 = ['E5', 'D#5', 'E5', '',  'B4', '', 'G#4', 'A4', 'B4',  '',  '', ''];
+  var melodyLine2 = ['D5', 'C#5', 'D5', '',  'E5', '', 'C#5', 'A4', 'B4',  '',  '', ''];
+  var melodyLine3 = ['E5', 'D#5', 'E5', '', 'F#5', '', 'G#5', 'E5', 'A5',  '',  '', ''];
+  
+  var fancyRhythm1 = [ 'D',   'T',  'T', 'D',   'T', 'D',   'T',   'T',   'T',  'D',   'T', 'Q', 'Q', 'Q'];
+  var melodyLine4 = [ 'A5', 'G#5', 'A5',  '', 'G#5',  '', 'F#5', 'G#5', 'F#5', 'E5', 'F#5',  '',  '', ''];
 
   var glueRhythmMelody = function(R, M) {
     for(var i = 0; i < R.length; i++) {
@@ -16,15 +20,17 @@ KK.melody.PWM1 = (function() {
       melody.push({
         frequency: frequency,
         volume: volume,
-        duration: KK[rhythm1[i]]
+        duration: KK[R[i]]
       });
     }
   }
 
   glueRhythmMelody(rhythm1, melodyLine1);
   glueRhythmMelody(rhythm1, melodyLine2);
-
-  console.log(melody);
+  glueRhythmMelody(rhythm1, melodyLine3);
+  glueRhythmMelody(fancyRhythm1, melodyLine4);
+  glueRhythmMelody(rhythm1, melodyLine1);
+  glueRhythmMelody(rhythm1, melodyLine2);
 
   return melody;
 })();
