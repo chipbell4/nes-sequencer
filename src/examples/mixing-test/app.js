37c58d80062cda0d0bdf460a0d5bd570123a3bc9
Chip Bell <cbell@pbs.org>
2016-05-14 11:51:14 -0400

    Double checking different pulse widths

--- src/examples/mixing-test/app.js
+++ src/examples/mixing-test/app.js
@@ -22,6 +22,8 @@ var MixingTest = {
 };
 
 MixingTest.melody.PWM1 = MixingTest.generateMelody(1, 440, 8);
+NES.Oscillators.setPulseWidth('PWM1', 0.25);
 MixingTest.melody.PWM2 = MixingTest.generateMelody(2, 440 * 2 / 3, 8);
+NES.Oscillators.setPulseWidth('PWM2', 0.5);
 MixingTest.melody.TRIANGLE = MixingTest.generateMelody(3, 440 / 4, 8);
 MixingTest.melody.NOISE = MixingTest.generateMelody(4, 1000, 8);
