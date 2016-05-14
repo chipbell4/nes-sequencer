82d07eedc16f4b84ab45fb3f0ece9f0ad824f21b
Chip Bell <cbell@pbs.org>
2016-05-14 17:05:28 -0400

    Fixing some code style for effects

--- src/effects.js
+++ src/effects.js
@@ -1,85 +1,85 @@
 module.exports = {
-  Staccato: function(melody, noteReduction) {
-    var noteReduction = noteReduction || 0.5;
-    var newMelody = [];
-    for(var i = 0; i < melody.length; i++) {
+  Staccato: function (melody, noteReduction) {
+    noteReduction = noteReduction || 0.5
+    var newMelody = []
+    for (var i = 0; i < melody.length; i++) {
       newMelody.push({
         frequency: melody[i].frequency,
         duration: melody[i].duration * noteReduction,
         volume: melody[i].volume
-      });
-      
+      })
+
       newMelody.push({
-        frequency: melody[i + 1] ? melody[i+1].frequency : melody[i].frequency,
+        frequency: melody[i + 1] ? melody[i + 1].frequency : melody[i].frequency,
         duration: melody[i].duration * (1 - noteReduction),
         volume: 0
-      });
+      })
     }
 
-    return newMelody;
+    return newMelody
   },
 
-  Arpeggio: function(frequencies, duration, startVolume, endVolume) {
-    endVolume = endVolume === undefined ? startVolume : endVolume;
-    var noteDuration = 1 / 60 * 1000; // 60Hz refresh rate on NES
-    var totalNotes = duration / noteDuration;
+  Arpeggio: function (frequencies, duration, startVolume, endVolume) {
+    endVolume = endVolume === undefined ? startVolume : endVolume
+    var noteDuration = 1 / 60 * 1000 // 60Hz refresh rate on NES
+    var totalNotes = duration / noteDuration
 
-    var melody = [];
-    for(var i = 0; i < totalNotes; i++) {
-      var volume = startVolume + (endVolume - startVolume) * i / totalNotes;
-      var frequencyIndex = i % frequencies.length;
+    var melody = []
+    for (var i = 0; i < totalNotes; i++) {
+      var volume = startVolume + (endVolume - startVolume) * i / totalNotes
+      var frequencyIndex = i % frequencies.length
 
       melody.push({
         frequency: frequencies[frequencyIndex],
         volume: volume,
         duration: noteDuration
-      });
+      })
     }
 
-    melody = this.ClampDuration(melody, duration);
+    melody = this.ClampDuration(melody, duration)
 
-    return melody;
+    return melody
   },
 
-  Vibrato: function(melody, vibratoFrequency, vibratoPercentage) {
-    var vibratoMelodyComponents = melody.map(function(note) {
-      var expanded = [];
+  Vibrato: function (melody, vibratoFrequency, vibratoPercentage) {
+    var vibratoMelodyComponents = melody.map(function (note) {
+      var expanded = []
 
-      for(var t = 0; t < note.duration; t += 50 ) {
-        var offsetRatio = 1 + vibratoPercentage * Math.sin(t / 1000 * 2 * Math.PI * vibratoFrequency);
+      for (var t = 0; t < note.duration; t += 50) {
+        var offsetRatio = 1 + vibratoPercentage * Math.sin(t / 1000 * 2 * Math.PI * vibratoFrequency)
         expanded.push({
           volume: note.volume,
           duration: 50,
-          frequency: note.frequency * offsetRatio,
-        });
+          frequency: note.frequency * offsetRatio
+        })
       }
 
-      melody = this.ClampDuration(expanded, note.duration);
+      melody = this.ClampDuration(expanded, note.duration)
 
       // correct any notes being too long
-      var totalLength = expanded.reduce(function(sum, vibratoNote) {
-        return sum + vibratoNote.duration;
-      }, 0);
+      var totalLength = expanded.reduce(function (sum, vibratoNote) {
+        return sum + vibratoNote.duration
+      }, 0)
 
-      var lastNoteReduction = totalLength - note.duration;
-      expanded[expanded.length - 1].duration -= lastNoteReduction;
+      var lastNoteReduction = totalLength - note.duration
+      expanded[expanded.length - 1].duration -= lastNoteReduction
 
-      return expanded;
-    }.bind(this));
+      return expanded
+    }.bind(this))
 
     // flatten
-    return Array.prototype.concat.apply([], vibratoMelodyComponents);
+    return Array.prototype.concat.apply([], vibratoMelodyComponents)
   },
 
-  ClampDuration: function(melody, duration) {
+  ClampDuration: function (melody, duration) {
     // correct any notes being too long
-    var totalLength = melody.reduce(function(sum, note) {
-      return sum + note.duration;
-    }, 0);
+    var totalLength = melody.reduce(function (sum, note) {
+      return sum + note.duration
+    }, 0)
 
-    var lastNoteReduction = totalLength - duration;
-    melody[melody.length - 1].duration -= lastNoteReduction;
+    var lastNoteReduction = totalLength - duration
+    melody[melody.length - 1].duration -= lastNoteReduction
 
-    return melody;
-  },
+    return melody
+  }
 }
