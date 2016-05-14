977e78e35eafc20d21ca759a77ead004b46d0d6b
Chip Bell <cbell@pbs.org>
2016-05-14 16:52:52 -0400

    Starting support for modules

--- src/nes-main.js
+++ src/nes-main.js
@@ -1,1 +1,11 @@
-var NES = {};
+(function() {
+  var NES = {};
+
+  if(typeof module !== 'undefined') {
+    module.exports = NES;
+  } else if(typeof window !== 'undefined') {
+    window.NES = NES;
+  } else {
+    throw Error('Nothing to attach to');
+  }
+})();
