fd8845af94635aac9167cd33c7d50e714f794303
Chip Bell <cbell@pbs.org>
2016-05-15 14:55:56 -0400

    First stab at mml support. Need to handle chords though

--- src/mml.js
+++ src/mml.js
@@ -1,2 +1,28 @@
+var MMLIterator = require('mml-iterator')
+
+var noteNumberToFrequency = function (noteNumber) {
+  // a440 is 69
+  var halftoneOffset = noteNumber - 69
+  return 440 * Math.pow(2, halftoneOffset / 12)
+}
+
 module.exports = {
+  mmlToMelody: function (mmlString) {
+    var iterator = new MMLIterator(mmlString)
+
+    var melody = []
+
+    var current = iterator.next()
+    while (current.value.type !== 'end') {
+      melody.push({
+        frequency: noteNumberToFrequency(current.value.noteNumber),
+        volume: current.value.velocity / 127,
+        cycles: Math.round(current.value.duration * 60)
+      })
+
+      current = iterator.next()
+    }
+
+    return melody
+  }
 }
