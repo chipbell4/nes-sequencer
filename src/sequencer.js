f61036ad5b27aea482929e46752a6145b603472a
Chip Bell <cbell@pbs.org>
2016-05-15 11:56:31 -0400

    Removing rounding for cycles

--- src/sequencer.js
+++ src/sequencer.js
@@ -1,6 +1,6 @@
 var Oscillators = require('./oscillators')
 
-var CYCLE_LENGTH_IN_MS = Math.round(1000 / 60)
+var CYCLE_LENGTH_IN_MS = 1000 / 60
 var sequencerInterval = null
 
 var convertDurationsToCycles = function (melody) {
