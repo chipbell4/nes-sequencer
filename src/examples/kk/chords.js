5f322950245a95fd90913810a64a8642cac7d85c
Chip Bell <cbell@pbs.org>
2016-06-20 13:52:53 -0400

    Stubbing out bridge chords

--- src/examples/kk/chords.js
+++ src/examples/kk/chords.js
@@ -1,39 +1,26 @@
 KK.melody.PWM2 = (function() {
-  var rest = function(cyclesName) {
-    return { frequency: 0, volume: 0, cycles: KK[cyclesName] };
-  };
+  //  'l4 r', // pickup note
+  //  'l1 rrrrrrrrrrrrrrrr',
+  var bridgeChordsMML = [
+    't' + KK.tempo,
+    'v20',
+    'l1',
+    '[g+ c d+]^', 
+    '[g+ b f]^',
+    '[f+ a+ c+ e]^',
+  ].join(' ');
 
-  var chords = {
-    E: ['E4', 'G#4', 'B4'].map(NES.MusicTools.frequency),
-    A: ['E4', 'A4', 'C#5'].map(NES.MusicTools.frequency),
-    D: ['F#4', 'A4', 'D5'].map(NES.MusicTools.frequency),
-    B: ['F#4', 'B4', 'D#5'].map(NES.MusicTools.frequency)
-  };
+  var bridgeTagMML = [
+    't' + KK.tempo,
+    'v20',
+    'l16',
+    '[b d+ f+] rrr',
+    '[b d+ f+] rrr',
+    '[e g > c] rrr',
+    '[e g > c] rrr',
+    '[b d+ f+] rrr',
+  ].join(' ');
 
-  var melody = [];
-
-  // Emaj lick
-  melody.push(rest('H'));
-  melody = melody.concat(NES.Effects.Arpeggio(chords.E, KK.H, 0.2, 0));
-
-  // Amaj
-  melody.push(rest('H'));
-  melody = melody.concat(NES.Effects.Arpeggio(chords.A, KK.H, 0.2, 0));
-
-  // Dmaj
-  melody.push(rest('H'));
-  melody = melody.concat(NES.Effects.Arpeggio(chords.D, KK.H, 0.2, 0));
-  
-  // Bmaj
-  melody.push(rest('H'));
-  melody = melody.concat(NES.Effects.Arpeggio(chords.B, KK.H, 0.2, 0));
-
-  // double up
-  melody = melody.concat(melody);
-
-  // push the pick-up note rest
-  melody.unshift(rest('Q'));
-
-  //return melody;
-  return [ rest('Q'), rest('Q') ];
+  var bridge = NES.Mml.mmlToMelody(bridgeChordsMML).concat(NES.Mml.mmlToMelody(bridgeTagMML));
+  return bridge;
 })();
