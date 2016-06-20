55164370b9f1839f76ba52bda2807db648f2bc81
Chip Bell <cbell@pbs.org>
2016-06-20 11:32:40 -0400

    Switching to mml! Much simpler!

--- src/examples/kk/melody.js
+++ src/examples/kk/melody.js
@@ -3,38 +3,16 @@ KK.melody.PWM1 = (function() {
     return { frequency: 0, volume: 0, cycles: KK[cyclesName] };
   };
 
-  var melody = [];
+  var mainThemeMML = [
+    't190',
+    '> e6 d+12 e < b g+ a6 b12 r r r',
+    '> d6 c+12 d e < > c+ < a6 b12 r r r',
+    '> e6 d+12 e f+ g+ e6 a12 r r r',
+    'a6 g+12 a g+ f+ e6 f+12 r r r',
+    'e6 d+12 e < b g+ a6 b12 r r r',
+    '> d6 c+12 d e < > c+ < a6 b12 r r r',
+    '> e6 d+12 e f+ g+ e6 a a12 g+ f+ e d+ e f+ d+6 e12 r r r',
+  ].join(' ');
 
-  var rhythm1 =     [ 'D',   'T', 'T', 'D',   'T', 'D',  'Q',  'D',  'T', 'Q', 'Q', 'Q'];
-  var melodyLine1 = ['E5', 'D#5', 'E5', '',  'B4', '', 'G#4', 'A4', 'B4',  '',  '', ''];
-  var melodyLine2 = ['D5', 'C#5', 'D5', '',  'E5', '', 'C#5', 'A4', 'B4',  '',  '', ''];
-  var melodyLine3 = ['E5', 'D#5', 'E5', '', 'F#5', '', 'G#5', 'E5', 'A5',  '',  '', ''];
-  
-  var fancyRhythm1 = [ 'D',   'T',  'T', 'D',   'T', 'D',   'T',   'T',   'T',  'D',   'T', 'Q', 'Q', 'Q'];
-  var melodyLine4 = [ 'A5', 'G#5', 'A5',  '', 'G#5',  '', 'F#5', 'G#5', 'F#5', 'E5', 'F#5',  '',  '', ''];
-
-  var rhythm2 =     [ 'D',   'T',  'T', 'D',   'T', 'D',   'T', 'D',  'D',  'T', 'D',  'T',  'T', 'D',   'T', 'D',  'T', 'D',   'T', 'D',  'T', 'D',   'T', 'D',   'D',  'T', 'D', 'Q', 'Q']; 
-  var melodyLine5 = ['E5', 'D#5', 'E5',  '', 'F#5',  '', 'G#5',  '', 'E5', 'A5',  '', 'A5', 'G#5', '', 'F#5',  '', 'E5', '',  'D#5',  '', 'E5',  '', 'F#5',  '', 'D#5', 'E5',  '',  '', '' ];
-
-  var glueRhythmMelody = function(R, M) {
-    for(var i = 0; i < R.length; i++) {
-      var volume = M[i] == '' ? 0.0 : 0.1;
-      var frequency = M[i] == '' ? 440 : NES.MusicTools.frequency(M[i]);
-      melody.push({
-        frequency: frequency,
-        volume: volume,
-        cycles: KK[R[i]]
-      });
-    }
-  }
-
-  glueRhythmMelody(rhythm1, melodyLine1);
-  glueRhythmMelody(rhythm1, melodyLine2);
-  glueRhythmMelody(rhythm1, melodyLine3);
-  glueRhythmMelody(fancyRhythm1, melodyLine4);
-  glueRhythmMelody(rhythm1, melodyLine1);
-  glueRhythmMelody(rhythm1, melodyLine2);
-  glueRhythmMelody(rhythm2, melodyLine5);
-
-  return melody;
+  return NES.Mml.mmlToMelody(mainThemeMML);
 })();
