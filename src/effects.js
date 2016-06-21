956e3d4dd112b010221496506a7214b2d9a85e43
Chip Bell <cbell@pbs.org>
2016-06-21 13:40:05 -0400

    Adding some comments for the effects module

--- src/effects.js
+++ src/effects.js
@@ -1,4 +1,15 @@
+/**
+ * Effects module
+ * @module NES.Effects
+ * @see NES
+ */
 module.exports = {
+  /**
+   * Takes a sequence of melody notes and converts them to a shorter, staccato representation
+   * @param {Array} melody The melody to shorten
+   * @param {Number} noteReduction The percentage (0 - 1) to reduce the note length by
+   * @return Array An updated melody, with shortened notes
+   */
   Staccato: function (melody, noteReduction) {
     noteReduction = noteReduction || 0.5
     var newMelody = []
@@ -20,6 +31,15 @@ module.exports = {
     return newMelody
   },
 
+  /**
+   * Takes a chord of frequencies and converts them into an arpeggiated chords that arpegiates at 60Hz (60 notes per
+   * second, the NES limit)
+   * @param {Array} frequencies An array of floating-point frequencies
+   * @param {Number} cycles The number of cycles to run
+   * @param {Number} startVolume The starting volume (0 - 1)
+   * @param {Number} endVolume The ending volume (0 - 1), defaults to the starting volume
+   * @return {Array} An array of notes that arpeggiates given the passed parameters
+   */
   Arpeggio: function (frequencies, cycles, startVolume, endVolume) {
     endVolume = endVolume === undefined ? startVolume : endVolume
 
@@ -38,6 +58,13 @@ module.exports = {
     return melody
   },
 
+  /**
+   * Adds vibrato to a given melody. Typical might look like this: NES.Effects.Vibrato(myMelody, 5, 0.015)
+   * @param {Array} melody The melody to add vibrato to
+   * @param {Number} vibratoFrequency The frequency at which to oscillate the note
+   * @param {Number} vibratoPercentage The percentage to vibrate by.
+   * @return {Array} An updated, vibrato melody
+   */
   Vibrato: function (melody, vibratoFrequency, vibratoPercentage) {
     var vibratoMelodyComponents = melody.map(function (note) {
       var melody = []
