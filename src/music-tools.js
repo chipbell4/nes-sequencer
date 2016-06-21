3da2472e4bd3d6ba618d8779ac448ae9ad0efb92
Chip Bell <cbell@pbs.org>
2016-06-21 13:48:02 -0400

    Some more comments

--- src/music-tools.js
+++ src/music-tools.js
@@ -1,8 +1,14 @@
+/**
+ * A few helpful functions for music-based calculations
+ * @module NES.MusicTools
+ * @see NES
+ */
 module.exports = {
-  duration: function (tempo, noteType) {
-    return (60 / tempo * noteType) * 1000
-  },
-
+  /**
+   * Calculates the frequency for a given note in scientific pitch notation
+   * @param {String} note A string to convert (like "A5", "Db3" or "G#4")
+   * @return {Number} The frequency of the note
+   */
   frequency: function (note) {
     var octave = Number(note[note.length - 1])
     var pitch = note.substr(0, note.length - 1)
@@ -35,31 +41,5 @@ module.exports = {
     var semitones = (octave - 4) * 12 + pitchOffset
     var c4 = 261.63
     return c4 * Math.pow(oneSemitone, semitones)
-  },
-
-  parse: function (note, tempo) {
-    var durationMap = {
-      W: 4,
-      H: 2,
-      Q: 1,
-      E: 0.5,
-      T: 0.33,
-      S: 0.25
-    }
-    var volumeMap = {
-      pp: 0.1,
-      p: 0.2,
-      mp: 0.3,
-      mf: 0.4,
-      f: 0.5,
-      ff: 0.6
-    }
-
-    var split = note.split(' ')
-    var frequency = this.frequency(split[0])
-    var duration = this.duration(tempo || 120, durationMap[split[1]] || 1)
-    var volume = volumeMap[2] || 0.5
-
-    return { frequency: frequency, duration: duration, volume: volume }
   }
 }
