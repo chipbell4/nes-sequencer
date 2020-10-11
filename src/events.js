242a1328c0044759bb0348bed7a300fcf547d0c7
Chip Bell <chipbell4@gmail.com>
2020-10-11 00:09:22 -0400

    Some small docs

--- src/events.js
+++ src/events.js
@@ -1,6 +1,14 @@
+/**
+ * @module NES.Events.Bus
+ */
 var bus = {
   listeners: {},
 
+  /**
+   * Triggers an event
+   * @param {String} name The name of the event to trigger
+   * @param {Object} data The data to emit with the event
+   */
   trigger: function(name, data) {
     if (this.listeners[name] === undefined) {
       return;
@@ -11,6 +19,11 @@ var bus = {
     }
   },
 
+  /**
+   * Registers an event listener
+   * @param {String} name The event to listen to
+   * @param {Function} callback The function to call when the event is emitted
+   */
   addEventListener: function(name, callback) {
     if (this.listeners[name] === undefined) {
       this.listeners[name] = [];
@@ -19,6 +32,11 @@ var bus = {
     this.listeners[name].push(callback);
   },
 
+  /**
+   * Removes a registered event listener
+   * @param {String} name The event the listener is attached to
+   * @param {Function} callback The function to remove
+   */
   removeEventListener: function(name, callback) {
     if (this.listeners[name] === undefined) {
       return;
@@ -39,7 +57,20 @@ var types = {
   SEQUENCER_TICK: 'SEQUENCER_TICK',
 };
 
+/**
+ * Utilities for emitting events
+ * @module NES.Events
+ */
 module.exports = {
+  /**
+   * An event bus that the sequencer uses to emit events
+   * @module NES.Events.Bus
+   */
   Bus: bus,
+
+  /**
+   * An enum of event types that get emitted
+   * @module NES.Events.Types
+   */
   Types: types,
 };
