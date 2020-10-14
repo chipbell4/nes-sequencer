50a90496d9419ec5f6bd757599373cf8576e8a2d
Chip Bell <chipbell4@gmail.com>
2020-10-13 21:22:09 -0400

    Finished the top line

--- src/examples/smb2/top.js
+++ src/examples/smb2/top.js
@@ -5,5 +5,12 @@ v25
 
 o5
 l12
-g f+6 f d6 < b a6 a- g4 > g4 < g4
+g f+6 f d6 < b a6 a- g4 > g4 < g2
+o5
+g6 c e6 g4 c e6 g < b > e- g b6 a a2
+g6 < b > d6 g4 < b > d6 g d- e g b6 a2 b >
+c6 < b > c6 < a4 > c < b6 a
+g6 f+ g6 e4 d- d6 e
+f6 e f6 < b4 > e d6 c c1
+
 `;
