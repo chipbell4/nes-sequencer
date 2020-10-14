8124f075e5a559a5c3212f951e1891d331b49ba0
Chip Bell <chipbell4@gmail.com>
2020-10-13 21:39:59 -0400

    Getting bass working

--- src/examples/smb2/app.js
+++ src/examples/smb2/app.js
@@ -1,4 +1,5 @@
 window.SMB2 = {
   PWM1: require('./top.js'),
   PWM2: require('./bottom.js'),
+  TRIANGLE: require('./bass.js'),
 };
---
+++ src/examples/smb2/bass.js
@@ -0,0 +1,18 @@
+module.exports =
+`
+t200
+v25
+o3
+l12
+d d-6 c < b6 g f6 e d4 > d4 < d4 g4
+
+l4
+c g c g
+< b > g < b > g
+< b- > g < b- > g
+< a > g < a > g
+f > f < f+ > f+ <
+g > g < a > a <
+d f < g > g
+c1
+`
