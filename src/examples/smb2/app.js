fdc73853f3c358253bfb93ecf9c3be920d8916e4
Chip Bell <chipbell4@gmail.com>
2020-10-13 21:45:11 -0400

    Drums now working in SMB2

--- src/examples/smb2/app.js
+++ src/examples/smb2/app.js
@@ -2,4 +2,5 @@ window.SMB2 = {
   PWM1: require('./top.js'),
   PWM2: require('./bottom.js'),
   TRIANGLE: require('./bass.js'),
+  NOISE: require('./drums.js'),
 };
---
+++ src/examples/smb2/drums.js
@@ -0,0 +1,15 @@
+module.exports =
+`
+t200
+v10
+r12 r4 r2 r1
+l24
+o5
+/:
+r4 c r r r r r
+r4 c r r r r r
+r4 c r r r r r
+r4 c r c r c r
+:/4
+
+`
